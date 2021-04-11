fd5d841cd5a0b4a544a9bd24148dbac0ba6c4f55
Aneesha Jocelyn DSouza <aneesha.jocelyn.dsouza@sap.com>
2021-04-11 15:32:17 +0530

    Task tracker

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+const Button = ({btnName, color, onClick}) => {
+    return (
+       
+           <button className='btn' style = {{backgroundColor:color}}
+           onClick ={onClick} > {btnName}</button> 
+        
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes ={
+    color: PropTypes.string,
+    btnName : PropTypes.string,
+    onClick: PropTypes.func
+}
+
+export default Button
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+import { useLocation } from "react-router-dom";
+
+const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      {location.pathname === "/" && (
+        <Button
+          btnName={showAddTask ? "Close" : "Add"}
+          color={showAddTask ? "red" : "Green"}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Header;
